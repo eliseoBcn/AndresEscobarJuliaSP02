afa3d4d9182e38c5d2d54dd1105e647edc15dcbd
andres escobar <eliseo.perez.casas@gmail.com>
2023-06-24 12:21:41 +0200

    COMMIT

---
+++ AndresEscobarJuliaSP02/src/main/java/n1ejercicio1/App.java
@@ -0,0 +1,18 @@
+package n1ejercicio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+ 
+    	
+    	Venta   venta = new Venta();
+    	
+    	
+    	
+    }
+}
